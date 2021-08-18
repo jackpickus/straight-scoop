f7d6e9d05093de17406e76a39f0b916b6fdea577
jackpickus <jpick329@gmail.com>
2021-08-18 16:56:37 -0500

    created about page

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const About: NextPage = () => {
+    return(
+        <div>
+            <Head>
+                <title>About - The Straight Scoop</title>
+            </Head>
+
+            <main>
+                <div>
+                    <p>Straight scoop is a sports/media site delivering key 
+                        insights and knowledge to sports fans around the country
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default About
