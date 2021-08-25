f157381bafa8c1e3edfff34a6224a2ae17cc3b30
jackpickus <jpick329@gmail.com>
2021-08-24 21:29:35 -0500

    created sports page

---
+++ pages/sports.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import NavLayout from "../components/navbar-layout";
+
+const sports: string[] = ["football", "basketball"];
+
+const Sports: NextPage = () => {
+  return (
+    <>
+      <NavLayout />
+      <main>
+        <div class="container mx-auto text-center">
+          <p class="text-4xl pb-2 underline font-semibold text-blue-500">
+            Sports
+          </p>
+          <ul>
+            {sports.map((value: string, index: number) => {
+              return (
+                <li key={index} class="py-2">
+                  <Link href={"/" + value}>
+                    <a class="hover:underline hover:text-blue-900">{value}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Sports;
